02722bca5e44ebb6d8fcd5e7a3858b24eb93a24a
bcotesta <bcotesta@gmail.com>
2025-10-27 20:46:02 -0400

    init everything

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// Brandon Cotesta | 10/27/2025 | 8:43 PM
+// MOOD: Neutral. not excited to spend the night doing this
+// but ready to get it done. Blue Jays are down 1-0 >:(.
+
 #include <QtCore/QCoreApplication>
 
 int main(int argc, char *argv[])
